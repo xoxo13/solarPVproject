08ab4d0858c15b44ba7a65d7fab6903b0ce30ab1
Ma Rosario Bernido <mbernido@asu.edu>
2019-03-27 02:38:09 -0700

    Create urls.py

---
+++ AppSolarPV/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('policy/', views.policy, name='policy'),
+    path('registration/', views.registration, name='registration'),
+    path('wpdash/', views.wpdash, name='wpdash')
+]
