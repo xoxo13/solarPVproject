47eff164d3134753526919ed1fb38861290f230b
Ma Rosario Bernido <mbernido@asu.edu>
2019-04-10 23:59:45 -0700

    My version

--- backend/admin.py
+++ backend/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from .models import Product, Certificate, Service
 
-
-admin.site.register(Product)
-admin.site.register(Certificate)
-admin.site.register(Service)
+# Register your models here.
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,23 +1,30 @@
 from rest_framework import serializers
+
 from ..models import Product, Certificate, Service
 
-class ProductSerializer(serializers.ModelSerializer):
 
-    class Meta:
-        model = Product
-        fields = ('modelNum', 'name', 'cellTechnology', 'cellManufacturer', 'numberOfCells',
-                    'numberOfCellsInSeries', 'numberOfSeriesStrings', 'numberOfDiodes', 'productLength',
-                    'productWidth', 'productWeigtht', 'superstrateType', 'superstrateManufacturer',
-                    'substrateType', 'substrateManufacturer', 'frameType', 'frameAdhesive',
-                    'encapsulantType', 'encapsulantManufacturer', 'junctionBoxType', 'junctionBoxManufacturer')
+class productSerializer(serializers.ModelSerializer): 
+	class Meta: 
+		model = Product
+			fields = ['modelNum', 'name', 'cellTechnology', 'cellManufacturer', 'numberOfCells', 
+		    'numberOfCellsInSeries', 'numberOfSeriesStrings','numberOfDiodes', 'productLength', 
+		    'productWidth', 'productWeigtht', 'superstrateType', 'superstrateManufacturer', 
+		    'substrateType', 'substrateManufacturer', 'frameType', 'frameAdhesive', 
+		    'encapsulantType', 'encapsulantManufacturer', 'junctionBoxType', 'junctionBoxManufacturer'
+			]
 
-class CertificateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Certificate
-        fields = ('cert_ID', 'certNumber', 'locationID',
-        'reportNumber', 'userID', 'testStandard', 'productID', 'certIssueDate')
+	
+class certificateSerializer(serializers.ModelSerializer): 
+	class Meta: 
+		model = Certificate
+			fields = ['cert_ID', 'certNumber', 'locationID', 'reportNumber', 
+			'userID', 'testStandard', 'productID', 'certIssueDate'
+			]
 
-class ServiceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Service
-        fields = ('serviceID', 'serviceName','description', 'isFIrequired', 'fiFrequency', 'testStandardID')
+
+class serviceSerializer(serializers.ModelSerializer): 
+	class Meta: 
+		model = Service
+			fields = ['serviceID', 'serviceName','description', 'isFIrequired', 
+			'fiFrequency', 'testStandardID' 
+			]
