7ebaf2304816805f65731069a9cde507e827e0bf
xoxo13 <12615672+xoxo13@users.noreply.github.com>
2019-04-28 12:50:31 -0700

    Update urls.py

--- AppSolarPV/urls.py
+++ AppSolarPV/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('policy/', views.policy, name='policy'),
     path('registration/', views.registration, name='registration'),
-    path('wpdash/', views.wpdash, name='wpdash')
+    path('wpdash/', views.wpdash, name='wpdash'),
      path('testcert/', views.testcert, name='testcert')
 ]
